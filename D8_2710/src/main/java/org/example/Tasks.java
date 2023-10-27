6cda732a1ff314be2c8785452cd9715b2f166e74
RenateLubaua <renate.tuce@gmail.com>
2023-10-27 18:42:02 +0100

    2710 Lekcija

--- D8_2710/src/main/java/org/example/Tasks.java
+++ D8_2710/src/main/java/org/example/Tasks.java
@@ -4,22 +4,18 @@ import java.util.Scanner;
 
 public class Tasks {
     public static void task1() {
+        Scanner sc = new Scanner(System.in);
         String[] ievade = new String[5];
 
         for ( int i=0; i<ievade.length; i++){
-            Scanner sc = new Scanner(System.in);
-
             System.out.println("Lūdzu ievadi savu frāzi");
             ievade[i] = sc.nextLine();
-
         }
-
 
         System.out.println("Ievadītās frāzes ir: ");
-        for (int i = 0; i < ievade.length; i++) {
 
+        for (int i = 0; i < ievade.length; i++) {
             System.out.print(ievade[i] + " ");
         }
-
     }
 }
