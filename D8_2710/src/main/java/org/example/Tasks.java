32af8058857491d8df6ac5e3aacb786eceda7b80
RenateLubaua <renate.tuce@gmail.com>
2023-10-27 18:55:32 +0100

    2710 Lekcija +uzd2

--- D8_2710/src/main/java/org/example/Tasks.java
+++ D8_2710/src/main/java/org/example/Tasks.java
@@ -3,6 +3,25 @@ package org.example;
 import java.util.Scanner;
 
 public class Tasks {
+
+    public static void task2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lūdzu ievadi vēlamo masīva garumu");
+        int garums = sc.nextInt();
+
+        int[] numMasīvs = new int[garums];
+
+        for (int i = 0; i < numMasīvs.length; i++) {
+            System.out.println("Lūdzu ievadi masīva datus");
+            numMasīvs[i] = sc.nextInt();
+        }
+
+        System.out.println("Ievadītie dati ir: ");
+
+        for (int i = 0; i < numMasīvs.length; i++) {
+            System.out.print(numMasīvs[i] + " ");
+        }
+    }
     public static void task1() {
         Scanner sc = new Scanner(System.in);
         String[] ievade = new String[5];
@@ -19,3 +38,4 @@ public class Tasks {
         }
     }
 }
+
