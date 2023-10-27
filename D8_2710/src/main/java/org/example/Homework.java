e3298ac611eaa499e4bfc043b36d2b6846635ae8
RenateLubaua <renate.tuce@gmail.com>
2023-10-27 20:26:37 +0100

    Mājas darbs bez bonusa

---
+++ D8_2710/src/main/java/org/example/Homework.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void Uzdevums1() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lūdzu ievadi vēlamo masīva garumu");
+        int garums = sc.nextInt();
+
+        int[] masīvs= new int[garums];
+
+        for (int i = 0; i < masīvs.length; i++) {
+            System.out.println("Lūdzu ievadi masīva datus");
+            masīvs[i] = sc.nextInt();
+        }
+
+        int skaitļi=masīvs.length;
+        int sum=0;
+        for (int i = 0; i < masīvs.length; i++) {
+            sum += masīvs[i];
+        }
+        double vidējais = sum/skaitļi;
+
+            System.out.println("Ievadīto skaitļu vidējā vērtība ir " + vidējais);
+
+            System.out.println("Ievadi, kādu skaitli vēlies atrast datu sistēmā");
+            int meklēt = sc.nextInt();
+
+        for (int i = 0; i < masīvs.length; i++) {
+            if (masīvs[i] == meklēt){
+                System.out.println("Meklētā skaitļa indekss ir " +i);
+            }
+        }
+
+
+    }
+}
+
--- D8_2710/src/main/java/org/example/Main.java
+++ D8_2710/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Tasks.task2();
+        Homework.Uzdevums1();
     }
 }
