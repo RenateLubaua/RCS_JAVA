03ff44b2c828067bd1e982b664b7169d624a3729
RenateLubaua <renate.tuce@gmail.com>
2023-10-30 18:53:35 +0000

    3010_Task2

--- D9_3010/src/main/java/org/example/Samples.java
+++ D9_3010/src/main/java/org/example/Samples.java
@@ -1,6 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Samples {
+
+    public static void sample2() {
+//Uztaisīt metodi, kas ar return padod masīvu, kuru ir ievadījis lietotājs
+
+        int[] a = genArr();
+        printArr(a);
+
+    }
+
+    private static int[] genArr() {
+        Scanner sc = new Scanner(System.in);
+        int[] numMasivs = new int[3];
+
+        for (int i=0; 1<numMasivs.length; i++){
+            System.out.println("Lūdzu ievadiet skaitli");
+            numMasivs[i]= sc.nextInt();
+        }
+        return numMasivs;
+
+    }
+
+
 
 
     public static void sample1(){
--- D9_3010/src/main/java/org/example/Uzdevumi.java
+++ D9_3010/src/main/java/org/example/Uzdevumi.java
@@ -1,8 +1,40 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Uzdevumi {
+
+
+public static void task2(){
+    Random rand = new Random();
+
+    int digits = 0;
+    for (int i=0; i<5; i++){
+        int a = rand.nextInt(7);
+        digits += a;
+        System.out.println(digits);
+    }
+
+    int[] array = new int[5];
+    for (int i=0; i<array.length; i++) {
+        array[i] = digits;
+        System.out.print(array[i] + " ");
+    }
+
+//    return printArr(array);
+
+
+}
+    private static void printArr(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+
+
 
     public static void task1() {
         Scanner sc = new Scanner(System.in);
@@ -12,7 +44,6 @@ public class Uzdevumi {
         for (int i = 0; i < array.length; i++) {
             System.out.println("Lūdzu ievadi masīva datus");
             array[i] = sc.nextLine();
-
         }
 
         symbols(array);
@@ -21,7 +52,7 @@ public class Uzdevumi {
     private static void symbols(String[] array) {
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            count += i.length;
+            count = count + array[i].length();
         }
         System.out.println("Ievadīto simbolu skaits ir: " +count);
 
